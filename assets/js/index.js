b7986f7ec467360a429d83d3f3a945b0f1bd53ec
fabrizio <faber.rvn@gmail.com>
2021-02-11 15:27:22 +0100

    #10 Fix style

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,79 +5,49 @@ document.querySelector('a[href$="' + subdomain + '"]').classList.add('active');
 
 $.ajax({
   type: "GET",
-  url: "../../resources/employees.json"
+  url: "../resources/employees.json"
 }).done(function(data) {
-// jsGrid Table
-$("#jsGrid").jsGrid({
-  width: "90%",
-  height: "400px",
+  // jsGrid Table
+  $("#jsGrid").jsGrid({
+    width: "100%",
+    height: "auto",
 
-  filtering: true,
-  inserting: true,
-  editing: true,
-  sorting: true,
-  paging: true,
-  autoload: true,
-  pageSize: 10,
-  pageButtonCount: 3,
-  deleteConfirm: "Do you really want to delay this data?",
-  controller: {
-    loadData: function(filter) {
-      return $.ajax({
-        url: "../../resources/employees.json",
-        dataType: "json"
-      });
-    }
-  },
-  fields: [
-    {
-      name: "id",
-      type: "hidden",
-      css: "hide"
-    },
-    {
-      name: "name",
-      type: "text",
-      width: 150,
-      validate: "required"
-    },
-    {
-      name: "lastName",
-      type: "text",
-      width: 150,
-      validate: "required"
-    },
-    {
-      name: "email",
-      type: "text",
-      width: 150,
-      validate: "required"
-    },
-    {
-      name: "age",
-      type: "text",
-      width: 50,
-      validate: function(value) {
-        if (value > 0) {
-          return true;
-        }
+    filtering: true,
+    inserting: true,
+    editing: true,
+    sorting: true,
+    paging: true,
+    autoload: true,
+    pageSize: 10,
+    pageButtonCount: 3,
+    deleteConfirm: "Do you really want to delay this data?",
+
+    controller: {
+      loadData: function(filter) {
+        return $.ajax({
+          url: "../resources/employees.json",
+          dataType: "json"
+        });
       }
     },
-    {
-      name: "gender",
-      type: "select",
-      items: [
-        { Name: "", Id: '' },
-        { Name: "Male", Id: 'male' },
-        { Name: "Female", Id: 'female' }
-      ],
-      valueField: "Id",
-      textField: "Name",
-      validate: "required"
-    },
-    {
-    type: "control"
-    }
-  ]
-})
+
+    fields: [
+      { name: "id", title: "Id", type: "number", width: 40 },
+      { name: "name", title: "First Name", type: "text", width: 100, validate: "required" },
+      { name: "lastName", title: "Last Name", type: "text", width: 120, validate: "required" },
+      { name: "email", title: "Email", type: "text", width: 150, validate: "required" },
+      { name: "age", title: "Age", type: "text", width: 50,
+        validate: value => { if (value > 0) return true; }
+      },
+      { name: "gender", title: "Gender", type: "select", width: 70,
+        items: [
+          { Name: "", Id: '' },
+          { Name: "man", title: "Man", Id: 'man' },
+          { Name: "woman", title: "Woman", Id: 'woman' }
+        ],
+        valueField: "Id", textField: "Name", validate: "required"
+      },
+      { type: "control" }
+    ]
+  })
 });

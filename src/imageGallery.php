a37825ed34f6da8c974bfd33c8dc98a8589e1f5e
Andres Auais <andresauais@gmail.com>
2021-02-16 12:29:43 +0000

    avatar images

--- src/imageGallery.php
+++ src/imageGallery.php
@@ -1,1 +1,15 @@
 <!-- TODO If you are going to add the extra feature implement here the image selection as a gallery or whatever you like -->
+<?php
+
+include'library/avatarsApi.php';
+// array from json
+$arrayFromJson =json_decode($response,true);
+// new avatar array
+$avatarArray =array();
+foreach($arrayFromJson as $element){
+  echo "<label><input type='radio' name='avatar' value=".$element['photo']."><img src=".$element['photo']." width='100px' /></label>";
+}
+// print_r($avatarArray);
+
+?>
+
---
+++ src/library/avatarsApi.php
@@ -0,0 +1,26 @@
+<?php
+
+$curl =curl_init();
+$url = 'https://uifaces.co/api?limit=5&gender[]=' . (isset($_GET['gender']) ? ($_GET['gender']== 'man' ? 'male' : 'female'): '');
+curl_setopt_array($curl,array(
+  CURLOPT_URL=> $url,
+  CURLOPT_RETURNTRANSFER=>true,
+  CURLOPT_ENCODING=>'',
+  CURLOPT_MAXREDIRS=>10,
+  CURLOPT_TIMEOUT=>0,
+  CURLOPT_FOLLOWLOCATION=>true,
+  CURLOPT_HTTP_VERSION=>CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST=>'GET',
+  CURLOPT_HTTPHEADER=>array(
+  'X-API-KEY: 9B934601-B7AF48A5-A5378686-95A45DBE',
+  'Accept: application/json\'',
+  'Cache-Control: no-cache',
+  'Cookie: PHPSESSID=5hgq38u2hp8rmi9ac11d5rni43'
+  ),
+));
+
+$response =curl_exec($curl);
+
+curl_close($curl);
+
+
